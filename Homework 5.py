a779a88514189eb661342e426ec2a7ef16502ce0
Krastti <salcev77@gmail.com>
2025-10-02 15:09:57 +0300

    Update Homework 5.py

--- Homework 5.py
+++ Homework 5.py
@@ -6,32 +6,28 @@ from matplotlib.animation import FuncAnimation
 # Параметры системы
 NUM_PARTICLES = 10
 TIME_STEP = 0.1
-BOUNDS = (0, 10, 0, 10)
 GRAVITY = np.array([0, -0.1], dtype=float)
-
-# Счетчик для статистики
 particles = []
+
 total_died = 0
 
 # Функция создания характеристик частицы
 def create_particle(pos, vel, lifetime):
-    return {
+    particle = {
         'pos': np.array(pos, dtype=float),
         'vel': np.array(vel, dtype=float),
         'lifetime': lifetime,
         'age': 0.0
     }
-
-# Функция добавления частицы
-def add_particle(pos, vel, lifetime):
-    particles.append(create_particle(pos, vel, lifetime))
+    return particle
 
 # Функция создания рандомный частицы
 def create_random_particle():
     pos = [random.uniform(0, 10), random.uniform(0, 10)]
     vel = [random.uniform(-1, 1), random.uniform(-1, 1)]
     lifetime = random.uniform(5, 15)
-    add_particle(pos, vel, lifetime)
+
+    particles.append(create_particle(pos, vel, lifetime))
 
 # Функция обновления системы
 def update_system(time_step):
@@ -39,10 +35,18 @@ def update_system(time_step):
     dead_count = 0
 
     for particle in particles:
+        ''' 
+        В данном случае реализована функция, о которой говорится в условии задачи, где нам сказано
+        
+        new_position = position + velocity * time_step
+                    
+        Однако, поскольку в системе присутствует гравитация, то для начала нужно просчитать скорость относительно гравитации
+        А после этого посчитать новую позицию.
+        '''
         particle['vel'] += GRAVITY * time_step
         new_pos = particle['pos'] + particle['vel'] * time_step
 
-        x_min, x_max, y_min, y_max = BOUNDS
+        x_min, x_max, y_min, y_max = 0, 10, 0, 10
         if new_pos[0] < x_min:
             new_pos[0] = x_min
             particle['vel'][0] = -particle['vel'][0]
@@ -70,12 +74,12 @@ def update_system(time_step):
     return dead_count
 
 # Функция позиции частицы
-def get_pos():
-    return np.array([p['pos'] for p in particles])
+def get_pos(particles_list):
+    return np.array([p['pos'] for p in particles_list])
 
-# Функция для вычисления врмени жизни частицы
-def get_remaining_lifetimes():
-    return np.array([p['lifetime'] - p['age'] for p in particles])
+# Функция для вычисления времени жизни частицы
+def get_remaining_lifetimes(particles_list):
+    return np.array([p['lifetime'] - p['age'] for p in particles_list])
 
 for _ in range(NUM_PARTICLES):
     create_random_particle()
@@ -88,15 +92,22 @@ ax.set_aspect('equal')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 
-initial_pos = get_pos()
-initial_lifetimes = get_remaining_lifetimes()
-scat = ax.scatter(initial_pos[:, 0], initial_pos[:, 1], c=initial_lifetimes, s=50, alpha=0.8, cmap='viridis')
+initial_pos = get_pos(particles)
+initial_lifetimes = get_remaining_lifetimes(particles)
+scat = ax.scatter(
+    initial_pos[:, 0],
+    initial_pos[:, 1],
+    c=initial_lifetimes,
+    s=50,
+    alpha=0.8,
+    cmap='inferno',
+)
 
 info_text = ax.text(0.02, 0.98, '', transform=ax.transAxes, verticalalignment='top', bbox=dict(boxstyle="round", facecolor="wheat", alpha=0.8))
 
 def init():
-    positions = get_pos()
-    lifetimes = get_remaining_lifetimes()
+    positions = get_pos(particles)
+    lifetimes = get_remaining_lifetimes(particles)
     scat.set_offsets(positions)
     scat.set_array(lifetimes)
     info_text.set_text(f'Кадр: 0\nУмерло: {total_died}')
@@ -105,8 +116,8 @@ def init():
 def animate(frame):
     update_system(TIME_STEP)
 
-    positions = get_pos()
-    lifetimes = get_remaining_lifetimes()
+    positions = get_pos(particles)
+    lifetimes = get_remaining_lifetimes(particles)
 
     if len(positions) > 0:
         scat.set_offsets(positions)
